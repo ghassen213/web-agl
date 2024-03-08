bf5171f623ebe660614ad55ad8a1bbb90f1b1a82
ghassen213 <161756770+ghassen213@users.noreply.github.com>
2024-03-08 17:20:31 +0100

    Update main.js

--- page principal/main.js
+++ page principal/main.js
@@ -36,15 +36,43 @@
         var isHeartFilled = element.classList.contains('fa-heart');
         var allHeartIcons = document.querySelectorAll('.offers i.fa-heart');
         
-        if (isHeartFilled) {
-          element.classList.remove('fa-heart');
-          element.classList.add('fa-heart-o');
-        } else {
-          for (var i = 0; i < allHeartIcons.length; i++) {
-            allHeartIcons[i].classList.remove('fa-heart');
-            allHeartIcons[i].classList.add('fa-heart-o');
-          }
-          element.classList.remove('fa-heart-o');
-          element.classList.add('fa-heart');
+      if (isHeartFilled) {
+        element.classList.remove('fa-heart');
+        element.classList.add('fa-heart-o');
+      } else {
+        for (var i = 0; i < allHeartIcons.length; i++) {
+          allHeartIcons[i].classList.remove('fa-heart');
+          allHeartIcons[i].classList.add('fa-heart-o');
         }
+        element.classList.remove('fa-heart-o');
+        element.classList.add('fa-heart');
       }
+    }
+
+
+
+    //remove sign in login button//
+    let clname = JSON.parse(sessionStorage.getItem("clname"));
+    let signUpButton = document.getElementById("signupButton");
+    let loginButton = document.getElementById("loginButton");
+    let navMenu = document.querySelector(".header-right .nav");
+
+    let test = JSON.parse(sessionStorage.getItem("test"));
+  
+    if (test.value == "yes") {
+      signUpButton.style.display = "none";
+      loginButton.style.display = "none";
+      navMenu.style.opacity = "1";
+      let navLink = document.querySelector(".header-right .nav__link");
+      navLink.innerHTML = `<i class="fa fa-user"></i>${clname.name} <i class="ri-arrow-down-s-line dropdown__arrow"></i>`;
+
+      
+    }
+
+function reset(){
+    sessionStorage.removeItem("test");
+  }
+
+
+
+
