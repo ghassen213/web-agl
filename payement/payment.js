d8f2453d9bfd785301d285d57410b05dc4614f51
ghassen213 <161756770+ghassen213@users.noreply.github.com>
2024-03-12 16:32:00 +0100

    Update payment.js

--- payement/payment.js
+++ payement/payment.js
@@ -1,27 +1,41 @@
 let checkOut = document.querySelector(".btn");
-let digits='1234567890'
+let inputs = document.querySelectorAll("input, select");
+let digits = '1234567890';
 
-let otp=''
+let otp = '';
 
-genOtp()
 function genOtp() {
-  otp=''
+  otp = '';
   for (let i = 0; i < 4; i++) {
-    otp+=digits[Math.floor(Math.random()*10)]
+    otp += digits[Math.floor(Math.random() * 10)];
   }
-  console.log(otp)
+  console.log(otp);
 }
 
 checkOut.addEventListener("click", (e) => {
-  e.preventDefault()
-  check = prompt(`Enter Otp.  ${otp}`)
-  if(check==otp){
-    alert("Thankyou for shopping.")
-    setTimeout(()=>window.location.href="/index.html",2000)
-    genOtp()
-  }else{
-    alert("Otp incorrect")
-    genOtp()
+  e.preventDefault();
+  let invalidFields = [];
+  inputs.forEach(input => {
+    if (!input.checkValidity()) {
+      invalidFields.push(input.getAttribute('placeholder'));
+    }
+  });
+  if (invalidFields.length === 0) {
+    let check = prompt(`Enter Otp: ${otp}`);
+    if (check === otp) {
+      alert("Thank you for shopping.");
+      setTimeout(() => window.location.href = "/index.html", 2000);
+      genOtp();
+    } else {
+      alert("Otp incorrect");
+      genOtp();
+    }
+    console.log(check);
+  } else {
+    let errorMsg = "Please correct the following input(s): \n";
+    invalidFields.forEach(field => {
+      errorMsg += `- ${field}\n`;
+    });
+    alert(errorMsg);
   }
-  console.log(check)
-})
+});
