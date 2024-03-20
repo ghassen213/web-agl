44ab4e2bbe3f60d3754513547936b140035ae541
ghassen213 <161756770+ghassen213@users.noreply.github.com>
2024-03-20 06:27:27 +0100

    Update signUp.php

--- signup-login/signUp.php
+++ signup-login/signUp.php
@@ -43,7 +43,7 @@ $_SESSION["verif"] = $verif;
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign-Up</title>
-    <link rel="stylesheet" href="SignUp.css">
+    <link rel="stylesheet" href="SignUp_SignIn.css">
 </head>
 <body>
     <div id="maincontainer">
