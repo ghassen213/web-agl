65b723e0dc11791736704beb3600f8b966e61001
ghassen213 <161756770+ghassen213@users.noreply.github.com>
2024-03-25 15:32:19 +0100

    Update payment.js

--- payement/payment.js
+++ payement/payment.js
@@ -1,36 +1,37 @@
+//******** ki t3amer cbon w tenzel tkharajlek alert feha code de verification lezem tektbou *********//
 let checkOut = document.querySelector(".btn");
 let inputs = document.querySelectorAll("input, select");
 let digits = '1234567890';
 
-let otp = '';
-
+let otp ='';
+genOtp()
 function genOtp() {
   otp = '';
   for (let i = 0; i < 4; i++) {
-    otp += digits[Math.floor(Math.random() * 10)];
+    otp += digits[Math.floor(Math.random() * 10)]
   }
-  console.log(otp);
 }
 
+
+//************ thabet mel code shih walle w thabet les input shah walle sinn ykharajlek alert ykolek chneya el ghalet ******//
 checkOut.addEventListener("click", (e) => {
-  e.preventDefault();
+
   let invalidFields = [];
   inputs.forEach(input => {
     if (!input.checkValidity()) {
-      invalidFields.push(input.getAttribute('placeholder'));
+      invalidFields.push(input.getAttribute('name'));
     }
   });
   if (invalidFields.length === 0) {
     let check = prompt(`Enter Otp: ${otp}`);
     if (check === otp) {
       alert("Thank you for shopping.");
-      setTimeout(() => window.location.href = "/index.html", 2000);
+      
       genOtp();
     } else {
-      alert("Otp incorrect");
+      alert("verification code incorrect");
       genOtp();
     }
-    console.log(check);
   } else {
     let errorMsg = "Please correct the following input(s): \n";
     invalidFields.forEach(field => {
